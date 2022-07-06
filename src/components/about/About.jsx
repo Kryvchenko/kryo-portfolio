587b1d931584be95ae47074093dc6438e2b150ab
Oleks Kryo <amzkryvchenko@gmail.com>
2022-07-06 20:41:07 -0300

    services experience about sections updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,46 @@
 import React from 'react' 
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image"/>
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Year Working Experience</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>10+ Worldwide</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>15+ Completed projects</small>
+            </article>
+           
+          </div>
+          <p>In early 2021 I started pursuing my passion and began developing skills in programming. I intend to pivot my career into web development.</p>
+            <a href='#contact' className='btn btn-primary'>Let`s Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
