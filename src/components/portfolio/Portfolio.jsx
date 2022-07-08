e42d1c70991ba49668c5a7785e646487aefe70b5
Oleks Kryo <amzkryvchenko@gmail.com>
2022-07-08 11:15:12 -0300

    photos added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,54 +1,31 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.png'
-import IMG6 from '../../assets/portfolio6.jpg'
+import IMG1 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.png'
+import IMG3 from '../../assets/portfolio3.png'
+
 
 const data = [
   {
     id:1,
     image: IMG1,
     title: "Motorcycle shop", 
-    github: "https://github.com",
+    github: "https://github.com/Kryvchenko/motoshop",
     demo: "https://dynamic-sorbet-13ccb2.netlify.app"
   },
   {
     id:2,
     image: IMG2,
-    title: "Motorcycle shop", 
-    github: "https://github.com",
-    demo: "https://dynamic-sorbet-13ccb2.netlify.app"
+    title: "Design studio", 
+    github: "https://github.com/Kryvchenko/kryo",
+    demo: "https://kryvchenko.github.io/kryo/"
   },
   {
     id:3,
     image: IMG3,
-    title: "Motorcycle shop", 
-    github: "https://github.com",
-    demo: "https://dynamic-sorbet-13ccb2.netlify.app"
-  },
-  {
-    id:4,
-    image: IMG4,
-    title: "Motorcycle shop", 
-    github: "https://github.com",
-    demo: "https://dynamic-sorbet-13ccb2.netlify.app"
-  },
-  {
-    id:5,
-    image: IMG5,
-    title: "Motorcycle shop", 
-    github: "https://github.com",
-    demo: "https://dynamic-sorbet-13ccb2.netlify.app"
-  },
-  {
-    id:6,
-    image: IMG6,
-    title: "Motorcycle shop", 
-    github: "https://github.com",
-    demo: "https://dynamic-sorbet-13ccb2.netlify.app"
+    title: "Volunteering organisation", 
+    github: "https://github.com/Kryvchenko/save-ukraine",
+    demo: "https://saveukrainelife.com/"
   },
 ]
 
