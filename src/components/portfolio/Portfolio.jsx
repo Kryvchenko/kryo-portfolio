137289ce2dd33506d1850584ab34a0cfe238e4a0
Oleksandr Kryvchenko <amzkryvchenko@gmail.com>
2022-09-20 17:27:51 +0200

    portfolio updated

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,30 +3,38 @@ import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.png'
 import IMG2 from '../../assets/portfolio2.png'
 import IMG3 from '../../assets/portfolio3.png'
+import IMG4 from '../../assets/portfolio4.png'
 import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 
 const data = [
   {
-    id:1,
+    id: 1,
     image: IMG1,
-    title: "Motorcycle shop", 
+    title: "Motorcycle shop",
     github: "https://github.com/Kryvchenko/motoshop",
     demo: "https://dynamic-sorbet-13ccb2.netlify.app"
   },
   {
-    id:2,
+    id: 2,
     image: IMG2,
-    title: "Design studio", 
+    title: "Design studio",
     github: "https://github.com/Kryvchenko/kryo",
     demo: "https://kryvchenko.github.io/kryo/"
   },
   {
-    id:3,
+    id: 3,
     image: IMG3,
-    title: "Volunteering organisation", 
+    title: "Volunteering organisation",
     github: "https://github.com/Kryvchenko/save-ukraine",
     demo: "https://saveukrainelife.com/"
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Personal portfolio",
+    github: "https://github.com/Kryvchenko",
+    demo: "https://lytovchenko.me/"
   },
 ]
 
@@ -41,22 +49,22 @@ const Portfolio = () => {
       <h2>P{text}<Cursor /></h2>
 
       <div className="container portfolio__container">
-       {
-        data.map(({id, image, title, github, demo}) => {
-          return (
-            <article key={id} className='portfolio__item'>
-            <div className="portfolio__item-image">
-              <img src={image} alt={title}/>
-            </div>
-            <h3>{title}</h3>
-             <div className="portfolio__item-cta">
-             <a href={github} className='btn' target='_blank' rel="noreferrer">Github</a>
-              <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
-             </div>
-          </article>  
-          )
-        })
-       }     
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank' rel="noreferrer">Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
