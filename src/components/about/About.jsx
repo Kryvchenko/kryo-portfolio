d2acdf78fcb4baeee99e3d1a678da977199f6662
Oleksandr Kryvchenko <amzkryvchenko@gmail.com>
2022-11-23 00:33:15 +0100

    contact info updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react' 
+import React from 'react'
 import './about.css'
 import ME from '../../assets/me-about.jpg'
 import { FaAward } from 'react-icons/fa'
@@ -11,40 +11,40 @@ const About = () => {
     words: ["bout Me"],
     loop: 0,
   });
-  
+
   return (
     <section id='about'>
       <h5>Get To Know</h5>
       <h2>A{text}
-                <Cursor /></h2>
+        <Cursor /></h2>
 
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={ME} alt="About"/>
+            <img src={ME} alt="About" />
           </div>
         </div>
         <div className="about__content">
           <div className="about__cards">
             <article className='about__card'>
-              <FaAward className='about__icon'/>
+              <FaAward className='about__icon' />
               <h5>Experience</h5>
               <small>1+ Year Working Experience</small>
             </article>
             <article className='about__card'>
-              <FiUsers className='about__icon'/>
+              <FiUsers className='about__icon' />
               <h5>Clients</h5>
               <small>10+ Worldwide</small>
             </article>
             <article className='about__card'>
-              <VscFolderLibrary className='about__icon'/>
+              <VscFolderLibrary className='about__icon' />
               <h5>Projects</h5>
               <small>15+ Completed projects</small>
             </article>
-           
+
           </div>
-          <p>In early 2021 I started pursuing my passion and began developing skills in programming. I intend to pivot my career into web development.</p>
-            <a href='#contact' className='btn btn-primary'>Let`s Talk</a>
+          <p>I have almost a decade of customer service experience. I think outside the box to come up with solutions alone and alongside a team. Have a strong creative side, which helps me be thoughtful about design and ways to make my projects user-friendly.</p>
+          <a href='#contact' className='btn btn-primary'>Let`s Talk</a>
         </div>
       </div>
     </section>
