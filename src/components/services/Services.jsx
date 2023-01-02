d0e21916d182ee8d8c902bc2a9618cd8fa6f1427
Oleksandr Kryvchenko <amzkryvchenko@gmail.com>
2023-01-02 19:49:51 +0100

    services and cv updated

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -16,6 +16,49 @@ const Services = () => {
       <div className="container services__container">
         <article className='service'>
           <div className='service__head'>
+            <h3>Quality Assurance</h3>
+          </div>
+          <ul className='service__list'>
+            <li><BiCheck className='service__list-icon '/>
+            <p>API testing </p>
+            </li>
+            <li><BiCheck className='service__list-icon '/>
+            <p>Writing test cases, scenarios </p>
+            </li>
+            <li><BiCheck className='service__list-icon '/>
+            <p>UI e2e testing </p>
+            </li>
+            <li><BiCheck className='service__list-icon '/>
+            <p>Defects reporting   </p>
+            </li>
+            <li><BiCheck className='service__list-icon '/>
+            <p>CI/CD </p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF QA*/}
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Web Development</h3>
+          </div>
+          <ul className='service__list'>
+            <li><BiCheck className='service__list-icon '/>
+            <p>Responsive web applications </p>
+            </li>
+            <li><BiCheck className='service__list-icon '/>
+            <p>Search engine optimization</p>
+            </li>
+            <li><BiCheck className='service__list-icon '/>
+            <p>Scalable, secure, maintainable code </p>
+            </li>
+            <li><BiCheck className='service__list-icon '/>
+            <p>Web hosting </p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF WEB DEVELOPMENT */}
+        <article className='service'>
+          <div className='service__head'>
             <h3>UI/UX Design</h3>
           </div>
           <ul className='service__list'>
@@ -33,50 +76,7 @@ const Services = () => {
             </li>
           </ul>
         </article>
-        {/* END OF UI/UX */}
-        <article className='service'>
-          <div className='service__head'>
-            <h3>Web Development</h3>
-          </div>
-          <ul className='service__list'>
-            <li><BiCheck className='service__list-icon '/>
-            <p>Responsive web applications </p>
-            </li>
-            <li><BiCheck className='service__list-icon '/>
-            <p>Search engine optimization</p>
-            </li>
-            <li><BiCheck className='service__list-icon '/>
-            <p>Automated system tests </p>
-            </li>
-            <li><BiCheck className='service__list-icon '/>
-            <p>Scalable, secure, maintainable code </p>
-            </li>
-            <li><BiCheck className='service__list-icon '/>
-            <p>Web hosting </p>
-            </li>
-          </ul>
-        </article>
-        {/* END OF WEB DEVELOPMENT */}
-        <article className='service'>
-          <div className='service__head'>
-            <h3>Content Creation </h3>
-          </div>
-          <ul className='service__list'>
-            <li><BiCheck className='service__list-icon '/>
-            <p>Research</p>
-            </li>
-            <li><BiCheck className='service__list-icon '/>
-            <p>Photography and Video </p>
-            </li>
-            <li><BiCheck className='service__list-icon '/>
-            <p>Text content creation </p>
-            </li>
-            <li><BiCheck className='service__list-icon '/>
-            <p>Metrics and analytics support </p>
-            </li>
-          </ul>
-        </article>
-        {/* END OF CONTENT CREATION */}
+        {/* END OF UI/UX  */}
       </div>
     </section>
   )
