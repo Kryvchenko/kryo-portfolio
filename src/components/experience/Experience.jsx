e054c78ea28378039495769627a3fa11445a7885
Oleksandr Kryvchenko <amzkryvchenko@gmail.com>
2023-01-09 12:01:24 +0100

    experience updated

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -129,7 +129,7 @@ const Experience = () => {
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
-                <h4>PHP</h4>
+                <h4>C#</h4>
                 <small className="text-light">Basic</small>
               </div>
             </article>
