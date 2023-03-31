c8f59b7f3ea1b399a807bcfe5e7052a8d10ae4c3
Oleksandr <94463783+Kryvchenko@users.noreply.github.com>
2023-03-31 17:14:43 +0200

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ import {FaFacebook} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://linkedin.com/in/oleksandr-kryvchenko/' target="_blank" rel="noreferrer"><BsLinkedin /></a>
+//         <a href='https://linkedin.com/in/oleksandr-kryvchenko/' target="_blank" rel="noreferrer"><BsLinkedin /></a>
         <a href='https://github.com/Kryvchenko' target="_blank" rel="noreferrer"><FaGithub /></a>
         <a href='https://facebook.com/kryvchenko1' target="_blank" rel="noreferrer"><FaFacebook /></a>
     </div>
